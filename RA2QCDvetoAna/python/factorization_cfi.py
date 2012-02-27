2185a6f6483932b6763d856b6c389fa591b56fe9
samantha <>
2012-02-27 15:47:05 +0000

    minor cleanup and formatting.

--- RA2QCDvetoAna/python/factorization_cfi.py
+++ RA2QCDvetoAna/python/factorization_cfi.py
@@ -9,8 +9,13 @@ factorization = cms.EDFilter(
                     #  outputFile = cms.string("Factorization"),
                       dMinHT = cms.untracked.double(0.0),   
                       dMinMHT = cms.untracked.double(0.0),   
+							 usePrescaleWeight = cms.untracked.int32(0),
+							 #prescaleWeight = cms.InputTag("prescaleweightProducer:weight"),
+							 prescaleWeight = cms.InputTag('trigWgtProd:prescaleWeight'),
                       verbose = cms.untracked.int32(0),
                       ApplyLumiWeighing = cms.untracked.int32(0),
-                      ApplyEventWeighing = cms.untracked.int32(0)
+                      ApplyEventWeighing = cms.untracked.int32(0),
+							 HltTriggerResults = cms.InputTag("TriggerResults::HLT"),
+							 TriggerPathsToStore = cms.vstring('HLT_HT*')
  )
 
