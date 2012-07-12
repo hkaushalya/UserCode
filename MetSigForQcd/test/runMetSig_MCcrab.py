1b8e0298cf6ea79eedb7d931ffdd65d22b0a210a
samantha <>
2012-07-12 21:57:18 +0000

    drivers for MetSig

--- MetSigForQcd/test/runMetSig_MCcrab.py
+++ MetSigForQcd/test/runMetSig_MCcrab.py
@@ -35,65 +35,166 @@ process.load('SandBox.Skims.RA2Selection_cff')
 process.mhtPFFilter.MinMHT = 0
 from SandBox.Skims.RA2Leptons_cff import *
 
-from UserCode.MetSigForQcd.metsig_cfi import *
+
+process.load("UserCode.MetSigForQcd.metsigall_cfi")
 
 usePrescaleWeight = 0
 applyLumiweighing = 0
 applyEventweighing = 0
 
-print 'usePrescaleWeight is set to  = ', usePrescaleWeight
-print 'applyLumiweighing is set to  = ', applyLumiweighing
-print 'applyEventweighing is set to = ', applyEventweighing
+print 'runMetSig_MCcrab: usePrescaleWeight is set to  = ', usePrescaleWeight
+print 'runMetSig_MCcrab: applyLumiweighing is set to  = ', applyLumiweighing
+print 'runMetSig_MCcrab: applyEventweighing is set to = ', applyEventweighing
 
-njet = 3
+njetmin = 6
+njetmax = 1000
 verbose = False
 njetCut = 1
 dPhiCut = 1
-dMinMetSig = 300
 
-process.metsignomht = metsig.clone(
-                      dMinMHT = cms.untracked.double(0.0),   
-                      dMinMetSig = cms.untracked.double(dMinMetSig),   
-							 dMinNjet = cms.untracked.double(njet),
-							 applyNjetCut = cms.untracked.int32(njetCut),
-							 applyDphiCut = cms.untracked.int32(dPhiCut),
-							 usePrescaleWeight = cms.untracked.int32(usePrescaleWeight),
-                      ApplyLumiWeighing = cms.untracked.int32(applyLumiweighing),
-                      ApplyEventWeighing = cms.untracked.int32(applyEventweighing)
-)
+print 'runMetSig_MCcrab:  njetmin = ', njetmin
+print 'runMetSig_MCcrab:  njetmax = ', njetmax
+print 'runMetSig_MCcrab:  njetcut = ', njetCut
+print 'runMetSig_MCcrab:  dPhiCut = ', dPhiCut
 
-process.metsigmht200 = metsig.clone(
-                      dMinMHT = cms.untracked.double(200.0),   
-                      dMinMetSig = cms.untracked.double(dMinMetSig),   
-							 dMinNjet = cms.untracked.double(njet),
-							 applyNjetCut = cms.untracked.int32(njetCut),
-							 applyDphiCut = cms.untracked.int32(dPhiCut),
-							 usePrescaleWeight = cms.untracked.int32(usePrescaleWeight),
-                      ApplyLumiWeighing = cms.untracked.int32(applyLumiweighing),
-                      ApplyEventWeighing = cms.untracked.int32(applyEventweighing)
-)
 
-process.metsigmht350 = metsig.clone(
-                      dMinMHT = cms.untracked.double(350.0),   
-                      dMinMetSig = cms.untracked.double(dMinMetSig),   
-							 dMinNjet = cms.untracked.double(njet),
-							 applyNjetCut = cms.untracked.int32(njetCut),
-							 applyDphiCut = cms.untracked.int32(dPhiCut),
-							 usePrescaleWeight = cms.untracked.int32(usePrescaleWeight),
-                      ApplyLumiWeighing = cms.untracked.int32(applyLumiweighing),
-                      ApplyEventWeighing = cms.untracked.int32(applyEventweighing)
-)
+process.metsignomht.dMinNjet = njetmin
+process.metsigmht25.dMinNjet = njetmin
+process.metsigmht50.dMinNjet = njetmin
+process.metsigmht75.dMinNjet = njetmin
+process.metsigmht100.dMinNjet = njetmin
 
-process.metsigmht500 = metsig.clone(
-                      dMinMHT = cms.untracked.double(500.0),   
-                      dMinMetSig = cms.untracked.double(dMinMetSig),   
-							 dMinNjet = cms.untracked.double(njet),
-							 applyNjetCut = cms.untracked.int32(njetCut),
-							 applyDphiCut = cms.untracked.int32(dPhiCut),
-							 usePrescaleWeight = cms.untracked.int32(usePrescaleWeight),
-                      ApplyLumiWeighing = cms.untracked.int32(applyLumiweighing),
-                      ApplyEventWeighing = cms.untracked.int32(applyEventweighing)
-)
+process.metsignomht.applyDphiCut = dPhiCut
+process.metsigmht25.applyDphiCut = dPhiCut
+process.metsigmht50.applyDphiCut = dPhiCut
+process.metsigmht75.applyDphiCut = dPhiCut
+process.metsigmht100.applyDphiCut = dPhiCut
+
+process.metsig50nomht.dMinNjet = njetmin
+process.metsig50mht25.dMinNjet = njetmin
+process.metsig50mht50.dMinNjet = njetmin
+process.metsig50mht75.dMinNjet = njetmin
+process.metsig50mht100.dMinNjet = njetmin
+
+process.metsig50nomht.applyDphiCut = dPhiCut
+process.metsig50mht25.applyDphiCut = dPhiCut
+process.metsig50mht50.applyDphiCut = dPhiCut
+process.metsig50mht75.applyDphiCut = dPhiCut
+process.metsig50mht100.applyDphiCut = dPhiCut
+
+process.metsig75nomht.dMinNjet = njetmin
+process.metsig75mht25.dMinNjet = njetmin
+process.metsig75mht50.dMinNjet = njetmin
+process.metsig75mht75.dMinNjet = njetmin
+process.metsig75mht100.dMinNjet = njetmin
+
+process.metsig75nomht.applyDphiCut = dPhiCut
+process.metsig75mht25.applyDphiCut = dPhiCut
+process.metsig75mht50.applyDphiCut = dPhiCut
+process.metsig75mht75.applyDphiCut = dPhiCut
+process.metsig75mht100.applyDphiCut = dPhiCut
+
+process.metsig100nomht.dMinNjet = njetmin
+process.metsig100mht25.dMinNjet = njetmin
+process.metsig100mht50.dMinNjet = njetmin
+process.metsig100mht75.dMinNjet = njetmin
+process.metsig100mht100.dMinNjet = njetmin
+
+process.metsig100nomht.applyDphiCut = dPhiCut
+process.metsig100mht25.applyDphiCut = dPhiCut
+process.metsig100mht50.applyDphiCut = dPhiCut
+process.metsig100mht75.applyDphiCut = dPhiCut
+process.metsig100mht100.applyDphiCut = dPhiCut
+
+
+
+#process.metsignomht.dMinNjet = njetmin
+#process.metsigmht100.dMinNjet = njetmin
+#process.metsigmht150.dMinNjet = njetmin
+#process.metsigmht175.dMinNjet = njetmin
+#process.metsigmht200.dMinNjet = njetmin
+#process.metsigmht350.dMinNjet = njetmin
+#process.metsigmht500.dMinNjet = njetmin
+#process.metsig100nomht.dMinNjet = njetmin
+#process.metsig100mht100.dMinNjet = njetmin
+#process.metsig100mht150.dMinNjet = njetmin
+#process.metsig100mht175.dMinNjet = njetmin
+#process.metsig100mht200.dMinNjet = njetmin
+#process.metsig100mht350.dMinNjet = njetmin
+#process.metsig100mht500.dMinNjet = njetmin
+#process.metsig200nomht.dMinNjet = njetmin
+#process.metsig200mht100.dMinNjet = njetmin
+#process.metsig200mht150.dMinNjet = njetmin
+#process.metsig200mht175.dMinNjet = njetmin
+#process.metsig200mht200.dMinNjet = njetmin
+#process.metsig200mht350.dMinNjet = njetmin
+#process.metsig200mht500.dMinNjet = njetmin
+#process.metsig300nomht.dMinNjet = njetmin
+#process.metsig300mht100.dMinNjet = njetmin
+#process.metsig300mht150.dMinNjet = njetmin
+#process.metsig300mht175.dMinNjet = njetmin
+#process.metsig300mht200.dMinNjet = njetmin
+#process.metsig300mht350.dMinNjet = njetmin
+#process.metsig300mht500.dMinNjet = njetmin
+#
+#process.metsignomht.dMaxNjet = njetmax
+#process.metsigmht100.dMaxNjet = njetmax
+#process.metsigmht150.dMaxNjet = njetmax
+#process.metsigmht175.dMaxNjet = njetmax
+#process.metsigmht200.dMaxNjet = njetmax
+#process.metsigmht350.dMaxNjet = njetmax
+#process.metsigmht500.dMaxNjet = njetmax
+#process.metsig100nomht.dMaxNjet = njetmax
+#process.metsig100mht100.dMaxNjet = njetmax
+#process.metsig100mht150.dMaxNjet = njetmax
+#process.metsig100mht175.dMaxNjet = njetmax
+#process.metsig100mht200.dMaxNjet = njetmax
+#process.metsig100mht350.dMaxNjet = njetmax
+#process.metsig100mht500.dMaxNjet = njetmax
+#process.metsig200nomht.dMaxNjet = njetmax
+#process.metsig200mht100.dMaxNjet = njetmax
+#process.metsig200mht150.dMaxNjet = njetmax
+#process.metsig200mht175.dMaxNjet = njetmax
+#process.metsig200mht200.dMaxNjet = njetmax
+#process.metsig200mht350.dMaxNjet = njetmax
+#process.metsig200mht500.dMaxNjet = njetmax
+#process.metsig300nomht.dMaxNjet = njetmax
+#process.metsig300mht100.dMaxNjet = njetmax
+#process.metsig300mht150.dMaxNjet = njetmax
+#process.metsig300mht175.dMaxNjet = njetmax
+#process.metsig300mht200.dMaxNjet = njetmax
+#process.metsig300mht350.dMaxNjet = njetmax
+#process.metsig300mht500.dMaxNjet = njetmax
+#
+#
+#process.metsignomht.applyDphiCut = dPhiCut
+#process.metsigmht100.applyDphiCut = dPhiCut
+#process.metsigmht150.applyDphiCut = dPhiCut
+#process.metsigmht175.applyDphiCut = dPhiCut
+#process.metsigmht200.applyDphiCut = dPhiCut
+#process.metsigmht350.applyDphiCut = dPhiCut
+#process.metsigmht500.applyDphiCut = dPhiCut
+#process.metsig100nomht.applyDphiCut = dPhiCut
+#process.metsig100mht100.applyDphiCut = dPhiCut
+#process.metsig100mht150.applyDphiCut = dPhiCut
+#process.metsig100mht175.applyDphiCut = dPhiCut
+#process.metsig100mht200.applyDphiCut = dPhiCut
+#process.metsig100mht350.applyDphiCut = dPhiCut
+#process.metsig100mht500.applyDphiCut = dPhiCut
+#process.metsig200nomht.applyDphiCut = dPhiCut
+#process.metsig200mht100.applyDphiCut = dPhiCut
+#process.metsig200mht150.applyDphiCut = dPhiCut
+#process.metsig200mht175.applyDphiCut = dPhiCut
+#process.metsig200mht200.applyDphiCut = dPhiCut
+#process.metsig200mht350.applyDphiCut = dPhiCut
+#process.metsig200mht500.applyDphiCut = dPhiCut
+#process.metsig300nomht.applyDphiCut = dPhiCut
+#process.metsig300mht100.applyDphiCut = dPhiCut
+#process.metsig300mht150.applyDphiCut = dPhiCut
+#process.metsig300mht175.applyDphiCut = dPhiCut
+#process.metsig300mht200.applyDphiCut = dPhiCut
+#process.metsig300mht350.applyDphiCut = dPhiCut
+#process.metsig300mht500.applyDphiCut = dPhiCut
 
 process.preseq = cms.Sequence(
                       process.ra2StdCleaning *
@@ -102,10 +203,27 @@ process.preseq = cms.Sequence(
 #                      process.ra2FullPFSelection *
 							 ra2PFMuonVeto *
 							 ra2PFElectronVeto *
-							 process.metsignomht +
-							 process.metsigmht200 +
-							 process.metsigmht350 +
-							 process.metsigmht500
+#							 process.metsigseq 
+							 process.metsignomht *
+							 process.metsigmht25 *
+							 process.metsigmht50 *
+							 process.metsigmht75 *
+							 process.metsigmht100 *
+							 process.metsig25nomht *
+							 process.metsig25mht25 *
+							 process.metsig25mht50 *
+							 process.metsig25mht75 *
+							 process.metsig25mht100 *
+							 process.metsig50nomht *
+							 process.metsig50mht25 *
+							 process.metsig50mht50 *
+							 process.metsig50mht75 *
+							 process.metsig50mht100 *
+							 process.metsig75nomht *
+							 process.metsig75mht25 *
+							 process.metsig75mht50 *
+							 process.metsig75mht75 *
+							 process.metsig75mht100 
 )
 
 
--- MetSigForQcd/test/testrunMetSig.py
+++ MetSigForQcd/test/testrunMetSig.py
@@ -1,7 +1,10 @@
 runningOnMC = True
 evts2Process = -1
-#outputFile="Data_1.root"
-outputFile="SUSYLM5_3jetsDphiMetSig300.root"
+outputFile="Data_1.root"
+#outputFile="SUSYLM9_2jetsDphi.root"
+#outputFile="SUSYLM9_2jetsOnly_lumiWgtd.root"
+#outputFile="SUSYLM9_2jetsOnly.root"
+#outputFile="SUSY_anwar_3jetsDphiMetSig300.root"
 
 import FWCore.ParameterSet.Config as cms
 
@@ -69,26 +72,68 @@ process.source = cms.Source("PoolSource",
 #'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM9SUSYsftshtpythia6_Summer11/susypat_24_1_myX.root',
 #'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM9SUSYsftshtpythia6_Summer11/susypat_8_1_43T.root',
 #'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM9SUSYsftshtpythia6_Summer11/susypat_9_1_z6d.root'
-
+#
 
 ##########Seema's LM5 sample
-'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_10_1_SBb.root',
-'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_11_1_Hkw.root',
-'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_1_1_gzT.root',
-'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_12_1_Stc.root',
-'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_13_1_se3.root',
-'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_14_1_Xzg.root',
-'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_15_1_ApF.root',
-'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_16_1_N1c.root',
-'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_17_1_pgW.root',
-'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_2_1_rGh.root',
-'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_3_1_SZr.root',
-'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_4_1_PZW.root',
-'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_5_1_B8G.root',
-'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_6_1_xKn.root',
-'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_7_1_zpK.root',
-'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_8_1_Aly.root',
-'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_9_1_rPo.root'
+#'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_10_1_SBb.root',
+#'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_11_1_Hkw.root',
+#'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_1_1_gzT.root',
+#'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_12_1_Stc.root',
+#'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_13_1_se3.root',
+#'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_14_1_Xzg.root',
+#'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_15_1_ApF.root',
+#'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_16_1_N1c.root',
+#'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_17_1_pgW.root',
+#'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_2_1_rGh.root',
+#'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_3_1_SZr.root',
+#'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_4_1_PZW.root',
+#'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_5_1_B8G.root',
+#'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_6_1_xKn.root',
+#'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_7_1_zpK.root',
+#'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_8_1_Aly.root',
+#'file:/eos/uscms/store/user/seema/SusyRA2Analysis/19September2011_2JetSkim_LM5SUSYsftshtpythia6_Summer11/susypat_9_1_rPo.root'
+
+#Anwar's new SUSY MC sample
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_10_1_WdZ.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_11_1_NAm.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_12_1_GLv.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_13_1_iPo.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_14_1_VJA.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_15_1_vbQ.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_16_1_NK0.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_17_1_IhE.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_18_1_Qdr.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_19_1_EFk.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_1_1_7Gk.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_20_1_a07.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_21_1_Mfl.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_22_1_e0A.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_23_1_oBS.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_24_1_bC4.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_25_1_xGz.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_26_1_JBb.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_27_1_0Et.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_28_1_MDe.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_29_1_6oy.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_2_1_ahD.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_30_1_Y7F.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_31_1_4Je.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_32_1_si9.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_33_1_Hzo.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_34_1_ZQZ.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_35_1_7c2.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_36_1_Vb9.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_37_1_wFv.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_38_1_7W6.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_39_1_K0c.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_3_1_UQR.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_40_2_sKu.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_4_1_FL3.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_5_1_9Jw.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_6_1_dre.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_7_1_txf.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_8_1_kTd.root',
+'/store/user/lpcjm/samantha/SUSY_1500_260_bhatti/samantha/SUSY_1500_260-AODSIM/samantha_SUSY_1500_260_bhatti_2Jets_noLeptonVeto/ccb8981a4f7138e417de052ad96dfa62/susy_pat_9_1_3Le.root'
 
 
 			)
@@ -144,16 +189,18 @@ from SandBox.Skims.mhtProducer_cfi import *
 process.mhtPF = mht.clone()
 process.mhtPF.JetCollection = cms.InputTag('patJetsAK5PFPt30')
 
-njet = 3
+njet = 2
+njetmax = 1000
 verbose = False
 njetCut = 1
 dPhiCut = 1
-dMinMetSig = 300
+dMinMetSig = 0
 
 process.metsignomht = metsig.clone(
                       dMinMHT = cms.untracked.double(0.0),   
                       dMinMetSig = cms.untracked.double(dMinMetSig),   
 							 dMinNjet = cms.untracked.double(njet),
+							 dMaxNjet = cms.untracked.double(njetmax),
 							 applyNjetCut = cms.untracked.int32(njetCut),
 							 applyDphiCut = cms.untracked.int32(dPhiCut),
 							 usePrescaleWeight = cms.untracked.int32(usePrescaleWeight),
@@ -166,6 +213,7 @@ process.metsigmht200 = metsig.clone(
                       dMinMHT = cms.untracked.double(200.0),   
                       dMinMetSig = cms.untracked.double(dMinMetSig),   
 							 dMinNjet = cms.untracked.double(njet),
+							 dMaxNjet = cms.untracked.double(njetmax),
 							 applyNjetCut = cms.untracked.int32(njetCut),
 							 applyDphiCut = cms.untracked.int32(dPhiCut),
 							 usePrescaleWeight = cms.untracked.int32(usePrescaleWeight),
@@ -179,6 +227,7 @@ process.metsigmht350 = metsig.clone(
                       dMinMHT = cms.untracked.double(350.0),   
                       dMinMetSig = cms.untracked.double(dMinMetSig),   
 							 dMinNjet = cms.untracked.double(njet),
+							 dMaxNjet = cms.untracked.double(njetmax),
 							 applyNjetCut = cms.untracked.int32(njetCut),
 							 applyDphiCut = cms.untracked.int32(dPhiCut),
 							 usePrescaleWeight = cms.untracked.int32(usePrescaleWeight),
@@ -192,6 +241,7 @@ process.metsigmht500 = metsig.clone(
                       dMinMHT = cms.untracked.double(500.0),   
                       dMinMetSig = cms.untracked.double(dMinMetSig),   
 							 dMinNjet = cms.untracked.double(njet),
+							 dMaxNjet = cms.untracked.double(njetmax),
 							 applyNjetCut = cms.untracked.int32(njetCut),
 							 applyDphiCut = cms.untracked.int32(dPhiCut),
 							 usePrescaleWeight = cms.untracked.int32(usePrescaleWeight),
@@ -203,11 +253,11 @@ process.metsigmht500 = metsig.clone(
 
 
 process.preseq = cms.Sequence(
-#							 process.postProcessingSeq *	 #only for Seema's datasets
+							 #process.postProcessingSeq *	 #only for Seema's datasets
                       #process.ra2StdCleaning * #exclude this when running on Seema's skims
                       #process.ra2PostCleaning * #exclude this when running on Seema's skims
-#                      process.ra2EcalTPFilter *
-#                      process.ra2FullPFSelection *
+                     #process.ra2EcalTPFilter *
+#                     process.ra2FullPFSelection *
 							 ra2PFMuonVeto *
 							 ra2PFElectronVeto *
 							 #process.mhtPF *
