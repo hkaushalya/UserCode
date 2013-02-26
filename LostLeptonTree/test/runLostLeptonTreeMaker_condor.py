250cb8ef6e298fe3266bc0bfee85510db0018c3e
samantha <>
2013-02-26 20:34:12 +0000

    made all my ntuples for the round 1 of 2012 analysis.

--- LostLeptonTree/test/runLostLeptonTreeMaker_condor.py
+++ LostLeptonTree/test/runLostLeptonTreeMaker_condor.py
@@ -7,13 +7,20 @@ import sys
 fileNameContainer = cms.untracked.vstring()
 file = sys.argv[3]
 
+print 'arg 4=' , sys.argv[4]
+print 'arg 5=' , sys.argv[5]
+print 'arg 6=' , sys.argv[6]
+print 'arg 7=' , sys.argv[7]
+DATASET = int(sys.argv[6])
+print 'dataset=' , DATASET
+
+
 infile = open (file, "r")
 for line in infile.readlines():
        line = line.rstrip('\n') # equivalent to Perl's chomp
        fileNameContainer.append (line)
 
 print fileNameContainer
-
 
 runningOnMC = False
 
@@ -34,16 +41,28 @@ process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 #Summer12_DR53X 53X 	START53_V7F::All
 
 if runningOnMC:
-	process.GlobalTag.globaltag = "START52_V11C::All"
+	process.GlobalTag.globaltag = "START53_V7F::All"
 else:
-	process.GlobalTag.globaltag = "FT_53_V6_AN2::All"
+	print 'In else'
+	if( DATASET == 1):
+		process.GlobalTag.globaltag = "FT_53_V6_AN2::All"
+	elif( DATASET == 2):
+		process.GlobalTag.globaltag = "FT_53_V6C_AN2::All"
+	elif DATASET == 3:
+		process.GlobalTag.globaltag = "FT_53_V6_AN2::All"
+	elif DATASET == 4:
+		process.GlobalTag.globaltag = "GR_P_V41_AN2::All"
+	elif DATASET == 5:
+		process.GlobalTag.globaltag = "GR_P_V42_AN2::All"
+
+
 print 'USING GT : ', process.GlobalTag.globaltag 
 
 #-- Message Logger ------------------------------------------------------------
 process.MessageLogger.categories.append('PATSummaryTables')
 process.MessageLogger.cerr.PATSummaryTables = cms.untracked.PSet(
     limit = cms.untracked.int32(10),
-    reportEvery = cms.untracked.int32(1)
+    reportEvery = cms.untracked.int32(1000)
     )
 process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 
@@ -55,27 +74,53 @@ process.source = cms.Source("PoolSource",
 			fileNames = fileNameContainer
 			#fileNames = cms.untracked.vstring(
        #'/store/user/lpcsusyhad/QCD_Pt_300to470_TuneZ2star_8TeV_pythia6_Summer12/samantha/QCD_Pt-300to470_TuneZ2star_8TeV_pythia6/Summer12_PU_S7_START52_V9_RA2_NoCuts_09Aug2012V1/f42b6bbcc7ea08f6809cc21efa861dac/susypat_261_1_RZ9.root'
-   )
+   #)
 )
 
 from UserCode.LostLeptonTree.lostleptontree_cfi import *
 process.treeMaker = demo.clone(
-	no_beamHaloFilter = cms.bool(True),
-	no_HBHENoiseFilter = cms.bool(True)
-
+	no_beamHaloFilter = cms.bool(False),
+	no_HBHENoiseFilter = cms.bool(False)
 )
 process.treeMaker.MCflag = runningOnMC
-process.treeMaker.DoPUReweight = False
+process.treeMaker.DoPUReweight = runningOnMC
 process.treeMaker.ApplyEventWeighing = False  #for flat sample 
+
+from UserCode.TrigFilter.trigprescaleWeight_cfi import *
+process.trigProd = trigPrescaleWgtProd.clone(
+		#hltPaths = cms.vstring('HLT_HT*', 'HLT_PFHT*'),
+		hltPaths = cms.vstring('HLT_HT*', 'HLT_PF*'),
+		debug =cms.int32(0)
+)
+
+process.load('SandBox.Skims.RA2Leptons_cff')
+process.LeptonVeto = cms.Sequence(
+		process.ra2PFMuonVeto *
+		process.ra2ElectronVeto
+		)
+
+
+from RA2Classic.WeightProducer.weightProducer_cfi import *
+
+#for this to work the CMMSW_SEARCH_PATH should be updated using 
+# setenv CMSSW_SEARCH_PATH mylocation:$CMSSW_SEARCH_PATH 
+#process.puWeight = weightProducer.clone(
+#	FileNamePUDataDistribution = cms.string("UserCode/LostLeptonTree/DataPileupHistogram_RA2Summer12_190456-208686_ABCD.root"),
+ # for summer 12 MC samples
+#	PU = cms.int32(3)
+#)
 
 
 process.preseq = cms.Sequence(
+				process.LeptonVeto *
+				process.trigProd * 
+				#process.puWeight *
 		      process.treeMaker 
 )
 
 ##-- Output module configuration ---------------------------------------
 process.TFileService = cms.Service("TFileService",
-  fileName = cms.string(sys.argv[4]+'_'+sys.argv[5]+'.root')
+  fileName = cms.string(sys.argv[7]+'/'+sys.argv[4]+'_'+sys.argv[5]+'.root')
 )
 
 process.ppf = cms.Path( process.preseq )
